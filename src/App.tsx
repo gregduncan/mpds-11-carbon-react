f49d5889d2f4606ee162ea13865143e758b451e3
Greg Duncan <mail@gregduncan.co.uk>
2023-06-07 11:18:02 +0100

    Converts MDS carbon v10 tokens to v11

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <div>
       <Button>Hello world</Button>
+      <Button kind="secondary">Secondary</Button>
+      <Button kind="tertiary">Tertiary</Button>
+      <Button kind="danger">Danger</Button>
+      <Button kind="danger--tertiary">Danger Tertiary</Button>
+      <Button kind="danger--ghost">Danger Ghost</Button>
+      <Button kind="ghost">Ghost</Button>
     </div>
   );
 }
